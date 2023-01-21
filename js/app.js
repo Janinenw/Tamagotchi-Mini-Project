9cde554514681a61e734d588c0974f5f498f5520
Janinenw <janine.wexler@gmail.com>
2023-01-21 17:36:32 -0500

    working hunger level bar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-console.log("test")
+// class Oop {
+//     constructor(name) {
+//       this.name = name
+//       this.hunger = 0
+//       this.sleepiness = 0
+//       this.boredom = 0;
+//       this.age = 
+//     }
+// }
+
+
+
+ hungerLevel = 100;
+ hungerInterval = setInterval(decreaseHungerBar, 1000);
+
+function decreaseHungerBar() {
+  hungerLevel -= 10;
+  document.getElementById("hunger-level").style.width = hungerLevel + "%";
+  if (hungerLevel <= 0) {
+    clearInterval(hungerInterval)
+  }
+}
+
+
+sleepinessLevel = 100;
+sleepinessInterval = setInterval(decreaseSleepinessBar, 1000);
+
+function decreasesleepinessBar() {
+ slepinessLevel -= 10;
+ document.getElementById("sleepiness-level").style.width = sleepinessLevel + "%";
+ if (sleepinessLevel <= 0) {
+   clearInterval(sleepinessInterval);
+ }
+}
+
