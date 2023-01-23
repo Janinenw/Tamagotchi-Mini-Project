57e2d926cddb7c2619c301531f63bf5858f1d553
Janinenw <janine.wexler@gmail.com>
2023-01-22 19:51:09 -0500

    bars hidden until after named

--- js/app.js
+++ js/app.js
@@ -1,184 +1,58 @@
+let tamagotchi;
+let hungerLevel = 100;
+let sleepinessLevel = 100;
+let boredomLevel = 100;
+
 class Tamagotchi {
     constructor(name) {
-    this.name = name;
-    this.hunger = 0;
-    this.sleepiness = 0;
-    this.boredom = 0;
-    this.age = 0;
+        this.name = name;
+        this.hunger = 0;
+        this.sleepiness = 0;
+        this.boredom = 0;
+        this.age = 0;
     }
-    }
-    function createTamagotchi() {
-        let name = document.getElementById("name-input").value;
-        let tamagotchi = new Tamagotchi(name);
-        console.log(tamagotchi);
-    }
-
-
-    document.getElementById("instructions-button").addEventListener("click", function() {
-        alert("instructions here")
-    });
-    
-
-// this makes bars run at correct interval, but need to add event
-//listener with anonymous function so these buttons only run when start is clicked
-
-//  hungerLevel = 100;
-//  hungerInterval = setInterval(decreaseHungerBar, 5000);
-
-// function decreaseHungerBar() {
-//   hungerLevel -= 10;
-//   document.getElementById("hunger-level").style.width = hungerLevel + "%";
-//   if (hungerLevel <= 0) {
-//     clearInterval(hungerInterval)
-//   }
-// }
-
-// sleepinessLevel = 100;
-// sleepinessInterval = setInterval(decreaseSleepinessBar, 5000);
-
-// function decreaseSleepinessBar() {
-// sleepinessLevel -= 10;
-// document.getElementById("sleepiness-level").style.width = sleepinessLevel + "%";
-// if (sleepinessLevel <= 0) {
-// clearInterval(sleepinessInterval);
-// }
-// }
-
-
-// boredomLevel = 100;
-// boredomInterval = setInterval(decreaseBoredomBar, 5000);
-
-// function decreaseBoredomBar() {
-// boredomLevel -= 10;
-// document.getElementById("boredom-level").style.width = boredomLevel + "%";
-// if (boredomLevel <= 0) {
-// clearInterval(boredomInterval);
-// }
-// }
-
-// });
-// let hungerLevel = 100;
-// let hungerInterval;
-// let sleepinessLevel = 100;
-// let sleepinessInterval;
-// let boredomLevel = 100;
-// let boredomInterval;
-
-// document.getElementById("start-button").addEventListener("click", function() {
-//   if(hungerInterval === undefined) {
-//     hungerInterval = setInterval(decreaseHungerBar, 5000);
-//   } else {
-//     clearInterval(hungerInterval);
-//     hungerInterval = undefined;
-//   }
-//   if(sleepinessInterval === undefined) {
-//     sleepinessInterval = setInterval(decreaseSleepinessBar, 5000);
-//   } else {
-//     clearInterval(sleepinessInterval);
-//     sleepinessInterval = undefined;
-//   }
-//   if(boredomInterval === undefined) {
-//     boredomInterval = setInterval(decreaseBoredomBar, 5000);
-//   } else {
-//     clearInterval(boredomInterval);
-//     boredomInterval = undefined;
-//   }
-// });
-
-// function decreaseHungerBar() {
-//   hungerLevel -= 10;
-//   document.getElementById("hunger-level").style.width = hungerLevel + "%";
-//   if (hungerLevel <= 0) {
-//     clearInterval(hungerInterval);
-//     hungerInterval = undefined;
-//   }
-// }
-
-// function decreaseSleepinessBar() {
-//   sleepinessLevel -= 10;
-//   document.getElementById("sleepiness-level").style.width = sleepinessLevel + "%";
-//   if (sleepinessLevel <= 0) {
-//     clearInterval(sleepinessInterval);
-//     sleepinessInterval = undefined;
-//   }
-// }
-
-// function decreaseBoredomBar() {
-//   boredomLevel -= 10;
-//   document.getElementById("boredom-level").style.width = boredomLevel + "%";
-//   if (boredomLevel <= 0) {
-//     clearInterval(boredomInterval);
-//     boredomInterval = undefined;
-//   }
-// }
-// function createTamagotchi() {
-//     let name = document.getElementById("name-input").value;
-//     let tamagotchi = new Tamagotchi(name);
-//     console.log(tamagotchi);
-// }
-
-
-
-
-let hungerLevel = 100;
-let hungerInterval;
-let sleepinessLevel = 100;
-let sleepinessInterval;
-let boredomLevel = 100;
-let boredomInterval;
+}
 
 document.getElementById("start-button").addEventListener("click", function() {
-  document.getElementById("name-input").style.display = "block";
-  if(hungerInterval === undefined) {
-    hungerInterval = setInterval(decreaseHungerBar, 5000);
-  } else {
-    clearInterval(hungerInterval);
-    hungerInterval = undefined;
-  }
-  if(sleepinessInterval === undefined) {
-    sleepinessInterval = setInterval(decreaseSleepinessBar, 5000);
-  } else {
-    clearInterval(sleepinessInterval);
-    sleepinessInterval = undefined;
-  }
-  if(boredomInterval === undefined) {
-    boredomInterval = setInterval(decreaseBoredomBar, 5000);
-  } else {
-    clearInterval(boredomInterval);
-    boredomInterval = undefined;
-  }
+    document.getElementById("name-form").style.display = "block";
 });
 
 function createTamagotchi() {
-  let name = document.getElementById("name-input").value;
-  let tamagotchi = new Tamagotchi(name);
-  console.log(tamagotchi);
+    let name = document.getElementById("name-input").value;
+    tamagotchi = new Tamagotchi(name);
+    console.log(tamagotchi);  
+    document.getElementById("hunger-bar").classList.remove("hidden");
+    document.getElementById("sleepiness-bar").classList.remove("hidden");
+    document.getElementById("boredom-bar").classList.remove("hidden");
+
+    let hungerInterval = setInterval(decreaseHungerBar, 5000);
+    let sleepinessInterval = setInterval(decreaseSleepinessBar, 5000);
+    let boredomInterval = setInterval(decreaseBoredomBar, 5000);
 }
 
 function decreaseHungerBar() {
-  hungerLevel -= 10;
-  document.getElementById("hunger-level").style.width = hungerLevel + "%";
-  if (hungerLevel <= 0) {
+    hungerLevel -= 10;
+    document.getElementById("hunger-level").style.width = hungerLevel + "%";
+    if (hungerLevel <= 0) {
     clearInterval(hungerInterval);
     hungerInterval = undefined;
-  }
+    }
 }
 
 function decreaseSleepinessBar() {
-  sleepinessLevel -= 10;
-  document.getElementById("sleepiness-level").style.width = sleepinessLevel + "%";
-  if (sleepinessLevel <= 0) {
+    sleepinessLevel -= 10;
+    document.getElementById("sleepiness-level").style.width = sleepinessLevel + "%";
+    if (sleepinessLevel <= 0) {
     clearInterval(sleepinessInterval);
     sleepinessInterval = undefined;
-  }
+    }
 }
 
 function decreaseBoredomBar() {
-  boredomLevel -= 10;
-  document.getElementById("boredom-level").style.width = boredomLevel + "%";
-  if (boredomLevel <= 0) {
+    boredomLevel -= 10;
+    document.getElementById("boredom-level").style.width = boredomLevel + "%";
+    if (boredomLevel <= 0) {
     clearInterval(boredomInterval);
     boredomInterval = undefined;
-  }
+    }
 }
-// as of now, power bars don't run until start, name input takes name and instantiates tama
