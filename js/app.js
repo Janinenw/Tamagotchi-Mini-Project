9385f52d148a07dcf69a34ef7b7dd0b96ab5984b
Janinenw <janine.wexler@gmail.com>
2023-01-23 22:55:52 -0500

    reworked, functions now connected to class, two out of three working properly, basic css

--- js/app.js
+++ js/app.js
@@ -1,119 +1,102 @@
 
-// let tamagotchi;
-let hungerLevel = 100;
-let sleepinessLevel = 100;
-let boredomLevel = 100;
-
 class Tamagotchi {
     constructor(name) {
         this.name = name;
-        this.hunger = 0;
-        this.sleepiness = 0;
-        this.boredom = 0;
+        this.hunger = 100;
+        this.sleepiness = 100;
+        this.boredom = 100;
         this.age = 0;
     }
 }
-document.getElementById("instructions-button").addEventListener("click", function() {
-   alert("instructions, but don't forget to actually type them")
-})
 
-document.getElementById("start-button").addEventListener("click", function() {
+function showNameForm() {
     document.getElementById("start-button").style.display = "none";
     document.getElementById("name-form").style.display = "block";
-    });
+}
 
-    function createTamagotchi() {
-        let name = document.getElementById("name-input").value;
-        tamagotchi = new Tamagotchi(name);
-        console.log(tamagotchi);  
-        document.getElementById("hunger-bar").classList.remove("hidden");
-        document.getElementById("sleepiness-bar").classList.remove("hidden");
-        document.getElementById("boredom-bar").classList.remove("hidden");
-        document.getElementById("action-buttons").style.display = "block"; 
-        document.getElementById("name-form").style.display = "none"; 
-        document.getElementById("feed-button").innerHTML = "Feed " + tamagotchi.name;
-        document.getElementById("sleep-button").innerHTML = "Put " + tamagotchi.name + " down for a nap"
-        document.getElementById("play-button").innerHTML = "Entertain " + tamagotchi.name;
-        let hungerInterval = setInterval(decreaseHungerBar, 5000);
-        let sleepinessInterval = setInterval(decreaseSleepinessBar, 5000);
-        let boredomInterval = setInterval(decreaseBoredomBar, 5000);
-    }
-    
+function createTamagotchi() {
+    let name = document.getElementById("name-input").value;
+    tamagotchi = new Tamagotchi(name);
+    console.log(tamagotchi);  
+    document.getElementById("hunger-bar").classList.remove("hidden");
+
+    document.getElementById("hunger-level").style.width = "100%";
+    document.getElementById("sleepiness-bar").classList.remove("hidden");
+    document.getElementById("sleepiness-level").style.width = "100%";
+    document.getElementById("boredom-bar").classList.remove("hidden");
+    document.getElementById("boredom-level").style.width = "100%";
+    document.getElementById("action-buttons").style.display = "block"; 
+    document.getElementById("name-form").style.display = "none"; 
+    document.getElementById("feed-button").innerHTML = "Feed " + tamagotchi.name;
+    document.getElementById("sleep-button").innerHTML = "Put " + tamagotchi.name + " down for a nap"
+    document.getElementById("play-button").innerHTML = "Entertain " + tamagotchi.name;
+
+
+let hungerInterval = setInterval(decreaseHungerBar, 5000);
+let sleepinessInterval = setInterval(decreaseSleepinessBar, 5000);
+let boredomInterval = setInterval(decreaseBoredomBar, 5000);
+}
 
 function decreaseHungerBar() {
-    hungerLevel -= 10;
-    document.getElementById("hunger-level").style.width = hungerLevel + "%";
-    if (hungerLevel <= 0) {
-    clearInterval(hungerInterval);
-    hungerInterval = undefined;
-    }
+  if(tamagotchi.hunger > 0) {
+  tamagotchi.hunger -= 10;
+  document.getElementById("hunger-level").style.width = tamagotchi.hunger + "%";
+} else {
+  clearInterval(hungerInterval);
+  hungerInterval = undefined;
+}
 }
 
 function decreaseSleepinessBar() {
-    sleepinessLevel -= 10;
-    document.getElementById("sleepiness-level").style.width = sleepinessLevel + "%";
-    if (sleepinessLevel <= 0) {
-    clearInterval(sleepinessInterval);
-    sleepinessInterval = undefined;
-    }
+  if(tamagotchi.sleepiness > 0) {
+  tamagotchi.sleepiness -= 10;
+document.getElementById("sleepiness-level").style.width = tamagotchi.sleepiness + "%";
+} else {
+  clearInterval(sleepinessInterval);
+  sleepinessInterval = undefined;
+}
 }
 
 function decreaseBoredomBar() {
-    boredomLevel -= 10;
-    document.getElementById("boredom-level").style.width = boredomLevel + "%";
-    if (boredomLevel <= 0) {
-    clearInterval(boredomInterval);
-    boredomInterval = undefined;
-    }
+  if(tamagotchi.boredom > 0) {
+  tamagotchi.boredom -= 10;
+  document.getElementById("boredom-level").style.width = tamagotchi.boredom + "%"
+} else {
+  clearInterval(boredomInterval);
+
+  boredomInterval = undefined;
+}
 }
 
 function increaseHungerBar() {
-    if(hungerLevel < 100) {
-        hungerLevel += 10;
-        document.getElementById("hunger-level").style.width = hungerLevel + "%";
-        if (hungerLevel >= 100) {
-        clearInterval(hungerInterval);
-        hungerInterval = undefined;
-        }
-    } else {
-
-    }
+if(tamagotchi.hunger < 100) {
+tamagotchi.hunger += 10;
+document.getElementById("hunger-level").style.width = tamagotchi.hunger + "%"
 }
-
+}
 
 function increaseSleepinessBar() {
-    if(sleepinessLevel < 100) {
-        sleepinessLevel += 10;
-        document.getElementById("sleepiness-level").style.width = sleepinessLevel + "%";
-        if (sleepinessLevel >= 100) {
-        clearInterval(sleepinessInterval);
-        sleepinessInterval = undefined;
-        }
-    } else {
-
+    if(tamagotchi.sleepiness < 100) {
+    tamagotchi.sleepiness += 10;
+    document.getElementById("sleepiness-level").style.width = tamagotchi.boredom + "%";
     }
-}
-
-
+    }
 function increaseBoredomBar() {
-    if(boredomLevel < 100) {
-        boredomLevel += 10;
-        document.getElementById("boredom-level").style.width = boredomLevel + "%";
-        if (boredomLevel >= 100) {
-        clearInterval(boredomInterval);
-        boredomInterval = undefined;
-        }
-    } else {
-    
+    if(tamagotchi.boredom < 100) {
+    tamagotchi.boredom += 10;
+    document.getElementById("boredom-level").style.width = tamagotchi.boredom + "%";
     }
-}
-// proper syntax for starting with tomagotchi.name within alert?  
-document.getElementById("feed-button").addEventListener("click", function() {
-    increaseHungerBar();
- });
- document.getElementById("sleep-button").addEventListener("click", function() {
-    increaseSleepinessBar();
- });
- document.getElementById("play-button").addEventListener("click", function() {
-    increaseBoredomBar();
- });
+    }
+
+    
+    document.getElementById("start-button").addEventListener("click", showNameForm);
+    document.getElementById("name-form").addEventListener("submit", function(event) {
+    event.preventDefault()
+    createTamagotchi()
+    });
+    
+document.getElementById("feed-button").addEventListener("click", increaseHungerBar)
+
+document.getElementById("sleep-button").addEventListener("click", increaseSleepinessBar)
+
+document.getElementById("play-button").addEventListener("click", increaseBoredomBar)
